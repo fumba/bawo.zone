17844457f6127220181fa8acc0eed67490d36423
panjie.dev <msowoya.panji@gmail.com>
2021-06-24 22:56:39 +0200

    added tests for padZero method

---
+++ test/Utility.test.ts
@@ -0,0 +1,34 @@
+/*
+ * bawo.zone - <a href="https://bawo.zone">https://bawo.zone</a>
+ * <a href="https://github.com/fumba/bawo.zone">https://github.com/fumba/bawo.zone</a>
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Utility from "../src/helpers/Utility";
+
+describe("#padZero", () => {
+  test("should try to add zero to a number that is less than 9", () => {
+    expect(Utility.padZero(2)).toBe("02");
+    expect(Utility.padZero(0)).toBe("00");
+    expect(Utility.padZero(9)).toBe("09");
+  });
+
+  test("should try not to add a zero if a number is not more than 9", () => {
+    expect(Utility.padZero(10)).toBe("10");
+    expect(Utility.padZero(15)).toBe("15");
+  });
+});
