57ebc3704bc6b97f742a4aedeb17f8496b212419
Panji Msowoya <38115401+panjie116@users.noreply.github.com>
2021-06-24 12:52:11 +0200

    moved the pad method from Hole into the utility class (#83)
    * moved the pad method from Hole into the utility class
    
    * utility class fix
    
    * removed console logs

--- src/helpers/Utility.ts
+++ src/helpers/Utility.ts
@@ -18,4 +18,10 @@
  * limitations under the License.
  */
 
-class Utility {}
+class Utility {
+  public static padZero(num: number): string {
+    const str = num.toString();
+    return str.length < 2 ? "0".concat(str) : str;
+  }
+}
+export default Utility;
