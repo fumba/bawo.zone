9de180506189ea9d251fd445e8c35e384866f49e
Fumba Chibaka <fumba.me@gmail.com>
2021-07-19 11:52:13 -0600

    fixup! add AI class

--- src/js/screens/PlayScreen.ts
+++ src/js/screens/PlayScreen.ts
@@ -7,9 +7,28 @@ import SeedGroupUI from "../ui_entities/SeedGroupUI";
 import UiHelper from "../ui_entities/UiHelper";
 import UiTaskActions from "../core/UiTaskActions";
 import Hole from "../core/Hole";
+import AI from "../core/AI";
 import Utility from "../Utility";
-import Move from "../core/Move";
 
+/*
+ * bawo.zone - <a href="https://bawo.zone">https://bawo.zone</a>
+ * <a href="https://github.com/fumba/bawo.zone">https://github.com/fumba/bawo.zone</a>
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 class PlayScreen extends me.Stage {
   private board: Board;
 
@@ -107,10 +126,7 @@ class PlayScreen extends me.Stage {
     me.timer.setInterval(() => {
       if (isCpuTopPlayerTurn) {
         isCpuTopPlayerTurn = false;
-        const moves: Array<Move> = this.board.getAllAvailableValidMoves(
-          this.board.getCurrentPlayer()
-        );
-        const move = moves[Utility.getRandomInt(moves.length)];
+        const move = AI.computeBestMove(this.board);
         this.board.executeMove(move);
       }
     }, gameSpeed);
