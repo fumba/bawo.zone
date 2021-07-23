96041517018c900aa682741dc27623870c67805e
Fumba Chibaka <fumba.me@gmail.com>
2021-07-23 10:19:58 -0600

    Added dev warning message (#207)

--- src/js/screens/title.ts
+++ src/js/screens/title.ts
@@ -1,11 +1,30 @@
 import me from "../me";
+import Utility from "../Utility";
 
 class TitleScreen extends me.Stage {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private betaLabel: any;
   /**
    *  action to perform on state change
    */
   onResetEvent(): void {
-    // TODO
+    me.game.world.addChild(new me.ColorLayer("background", "#b7b6b5"), -1);
+
+    this.betaLabel = new me.Text(100, 100, {
+      font: "Arial",
+      size: 15,
+      fillStyle: "#8B0000",
+    });
+    this.betaLabel.setText(
+      `This game is under development by Fumba Game Labs - this is an unfinished developers build. 
+      \nPlease check www.bawo.zone for development updates.
+      \n\n\nfumba.me@gmail.com - July 2021`
+    );
+    me.game.world.addChild(this.betaLabel);
+
+    Utility.sleep(4000).then(() => {
+      me.state.change(me.state.PLAY);
+    });
   }
 
   /**
